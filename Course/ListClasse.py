08fe8f09ae649850b4f42f199760d73b557f47b6
pashamsk <p.g1adyshev@yandex.ru>
2021-11-13 17:40:23 +0300

    refactored function __add__ and __sub__

--- Course/ListClasse.py
+++ Course/ListClasse.py
@@ -5,44 +5,10 @@ class MyList(list):
         self.my_list = lst
 
     def __add__(self, other):
-        dif = len(self.my_list) - len(other)
-        if dif == 0:
-            return MyList.my_add(self.my_list, other)
-        elif dif > 0:
-            tmp = other
-            tmp.extend([0] * dif)
-            return MyList.my_add(self.my_list, tmp)
-        else:
-            tmp = self.my_list
-            tmp.extend([0] * abs(dif))
-            return MyList.my_add(tmp, other)
-
-    def my_add(self, other):
-        add_list = MyList()
-        for _ in range(len(self)):
-            res = self[_] + other[_]
-            add_list.append(res)
-        return add_list
+        return MyList.construct(self, other, flag=1)
 
     def __sub__(self, other):
-        dif = len(self.my_list) - len(other)
-        if dif == 0:
-            return MyList.my_sub(self.my_list, other)
-        elif dif > 0:
-            tmp = other
-            tmp.extend([0] * dif)
-            return MyList.my_sub(self.my_list, tmp)
-        else:
-            tmp = self.my_list
-            tmp.extend([0] * abs(dif))
-            return MyList.my_sub(tmp, other)
-
-    def my_sub(self, other):
-        sub_list = MyList()
-        for _ in range(len(self)):
-            res = self[_] - other[_]
-            sub_list.append(res)
-        return sub_list
+        return MyList.construct(self, other, flag=0)
 
     def __eq__(self, other):
         if len(self.my_list) == len(other):
@@ -51,13 +17,36 @@ class MyList(list):
                     return False
         return True
 
+    def construct(self, other, flag):
+        dif = len(self.my_list) - len(other)
+        if dif == 0:
+            return MyList.my_meth(self.my_list, other, flag)
+        elif dif > 0:
+            tmp = other
+            tmp.extend([0] * dif)
+            return MyList.my_meth(self.my_list, tmp, flag)
+        else:
+            tmp = self.my_list
+            tmp.extend([0] * abs(dif))
+            return MyList.my_meth(tmp, other, flag)
+
+    def my_meth(self, other, flag):
+        sub_list = MyList()
+        for _ in range(len(self)):
+            if flag == 0:
+                res = self[_] - other[_]
+            else:
+                res = self[_] + other[_]
+            sub_list.append(res)
+        return sub_list
 
 v = [2, 4, 7]
 a = MyList([1, 3, 4, 5, 7])
 b = MyList([5, 6, 8])
 d = MyList([5, 6, 8])
 c = a + v
-print(c)
+e = a + b
+print(c, e)
 print(b - a)
 print(isinstance(c, MyList))
 print(b == d)
