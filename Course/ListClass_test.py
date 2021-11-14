28679a9ac8b3cb78786cb861833d27c34d1afc10
pashamsk <p.g1adyshev@yandex.ru>
2021-11-14 14:44:29 +0300

    added structure

---
+++ Course/ListClass_test.py
@@ -0,0 +1,18 @@
+from unittest import TestCase, main
+from ListClasse import MyList
+
+
+class TestListClass(TestCase):
+
+    def test_add(self):
+        pass
+
+    def test_sub(self):
+        pass
+
+    def test_eq(self):
+        pass
+
+
+if __name__ == '__main__':
+    main()
