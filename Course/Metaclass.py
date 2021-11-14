73b6818b4bfca5c0a4dd310e3e85c7f79247290f
pashamsk <p.g1adyshev@yandex.ru>
2021-11-14 14:37:53 +0300

    add tests, everything works

--- Course/Metaclass.py
+++ Course/Metaclass.py
@@ -2,40 +2,29 @@ class CustomMeta(type):
 
     def __call__(cls, *args, **kwargs):
         new_instance = super().__call__(*args, **kwargs)
-        new_attr = {}
-        for name, val in new_instance.__dict__.items():
-            if not (name.startswith('__') and name.endswith('__')):
-                new_attr['custom_' + name] = val
-        else:
-            new_attr[name] = val
-        new_instance.__dict__ = new_attr
+        new_instance.__dict__ = CustomMeta.new_attribute(new_instance.__dict__)
         return new_instance
 
     def __new__(mcs, user_class_name,
                 user_class_parents, user_class_attr):
-        new_attr = {}
-        for name, val in user_class_attr.items():
-            if not (name.startswith('__') and name.endswith('__')):
-                new_attr['custom_' + name] = val
-            else:
-                new_attr[name] = val
-        return type.__new__(mcs, user_class_name,
-                            user_class_parents, new_attr)
+        new_attr = CustomMeta.new_attribute(user_class_attr)
+        return super(CustomMeta, mcs).__new__(mcs, user_class_name,
+                                              user_class_parents, new_attr)
 
-    def new_attribute(cls, param):
+    @staticmethod
+    def new_attribute(param):
         new_attr = {}
         for name, val in param.items():
             if not (name.startswith('__') and name.endswith('__')):
                 new_attr['custom_' + name] = val
             else:
                 new_attr[name] = val
-        return
+        return new_attr
 
 
 class CustomClass(metaclass=CustomMeta):
     x = 50
-
-    # __c = 70
+    __y = 70
 
     def __init__(self, val=99):
         self.val = val
@@ -46,5 +35,10 @@ class CustomClass(metaclass=CustomMeta):
 
 
 a = CustomClass()
+print(CustomClass.__dict__)
 print(hasattr(a, 'custom_x'))
 print(hasattr(a, 'x'))
+print(hasattr(CustomClass, 'custom_line'))
+print(hasattr(CustomClass, 'custom_x'))
+print(hasattr(CustomClass, 'line'))
+print(hasattr(CustomClass, 'x'))
