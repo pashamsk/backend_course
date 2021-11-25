e1f5962b9aa5f9f7446b90e9e99e7e57737fdd3c
pashamsk <p.g1adyshev@yandex.ru>
2021-11-25 20:58:40 +0300

    makemigrations

--- messenger/users/models.py
+++ messenger/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class User(models.Model):
     name = models.CharField(max_length=64, verbose_name='имя пользователя')
     nick = models.CharField(max_length=64, unique=True, verbose_name='ник пользователя')
